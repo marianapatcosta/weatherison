8a72c407f49b6c53ae0bfb4c183ea999f17b8689
Mariana Costa <43031902+marianapatcosta@users.noreply.github.com>
2021-05-06 21:33:14 +0100

    add app footer

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,10 +1,38 @@
 <template>
   <div class="app">
     <Header />
-    <main role="main">
-     <!--  <router-view /> -->
-     <WeatherCompare/>
+    <main class="app__main" role="main">
+      <!--  <router-view /> -->
+      <WeatherCompare />
     </main>
+    <footer class="app__footer">
+      <p>
+        {{ `©2021 - ${$t("footer.developedBy")} ${$t("footer.authorName")}` }}
+      </p>
+      <a
+        class="app__footer-link"
+        :href="$t('footer.github')"
+        :aria-label="`go to ${$t('footer.authorName')}'s Github`"
+        target="_blank"
+        rel="nofollow noopener noreferrer"
+      >
+        <font-awesome-icon
+          :icon="['fab', 'github']"
+          :style="{ color: '#555555' }"
+        />
+      </a>
+      <a
+        class="app__footer-link"
+        :href="$t('footer.linkedin')"
+        target="_blank"
+        rel="nofollow noopener noreferrer"
+      >
+        <font-awesome-icon
+          :icon="['fab', 'linkedin']"
+          :style="{ color: '#555555' }"
+        />
+      </a>
+    </footer>
   </div>
 </template>
 
